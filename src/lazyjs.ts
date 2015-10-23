0406d141431c6d7d93b0a154bd0436bb1f9e85fe
Rodney Dennis <rod@carbonmade.com>
2015-10-23 17:43:35 -0500

    initial commit

---
+++ src/lazyjs.ts
@@ -0,0 +1,57 @@
+module Carbon {
+  export class LazyLoader {
+    lazyEls: NodeListOf<HTMLElement>;
+    fold: number;
+
+    constructor() {
+      window.addEventListener('scroll', this.check.bind(this));
+    }
+
+    setup() {
+      this.lazyEls = <NodeListOf<HTMLElement>>document.querySelectorAll('.lazy');
+      this.check();
+    }
+
+    check() {
+      if (!this.lazyEls) return;
+
+      this.fold = window.innerHeight;
+
+      for (var i = 0; i < this.lazyEls.length; i++) {
+        var el = this.lazyEls[i];
+
+        if (!el.classList.contains('lazy')) continue;
+
+        var box = el.getBoundingClientRect();
+
+        if(box.top <= this.fold + 500) {
+          this.load(el);
+
+          if ((i + 2) < this.lazyEls.length) {
+            var nextEl = this.lazyEls[i + 1];
+
+            this.load(nextEl);
+          }
+        }
+      }
+    }
+
+    load(el: HTMLElement) {
+      var src = el.getAttribute('data-src');
+      if (!src) {
+        throw new Error('[Carbon.LazyLoader] No source found for element');
+      }
+
+      var img = new Image();
+
+      img.onload = function() {
+        el.style.backgroundImage = "url('" + src + "')";
+        el.classList.add('loaded');
+      }
+
+      img.src = src;
+
+      el.classList.remove('lazy');
+    }
+  }
+}
